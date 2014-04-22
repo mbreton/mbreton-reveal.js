5fc8bfbfafe246560eca45dc9e394bdeb4782f1b
mbreton <mbreton@xebia.fr>
2014-04-23 00:39:28 +0200

    Fix editor & improve loading

--- plugin/sample-loader/loader.js
+++ plugin/sample-loader/loader.js
@@ -1,12 +1,14 @@
 (function(){
     var codeElements = document.querySelectorAll('[data-load]');
     Array.prototype.forEach.call(codeElements, function (codeElement) {
+        var fileName = codeElement.getAttribute('id');
         var promise = $.ajax({
-            url: '/samples/' + codeElement.getAttribute('id'),
+            url: '/samples/' + fileName,
             dataType: 'text'
         });
         promise.done(function (content) {
-            codeElements.innerHTML = content;
+            console.log(fileName + " loaded")
+            codeElement.innerHTML = content;
         });
     });
 })();
