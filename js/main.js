cc0ae6a6b14484739599f0a126aa10a2aca50898
mbreton <mbreton@xebia.fr>
2014-04-23 02:49:22 +0200

    Add implicit slide hooks

--- js/main.js
+++ js/main.js
@@ -1,34 +1,50 @@
-// Full list of configuration options available here:
-// https://github.com/hakimel/reveal.js#configuration
-Reveal.initialize({
-    controls: true,
-    progress: true,
-    history: true,
-    center: true,
+(function () {
+    Reveal.initialize({
+        controls: true,
+        progress: true,
+        history: true,
+        center: true,
 
-    // Optional libraries used to extend on reveal.js
-    dependencies: [
-        { src: 'plugin/sample-loader/loader.js', async: false},
-        { src: 'lib/js/classList.js', condition: function () {
-            return !document.body.classList;
-        } },
-        { src: 'plugin/markdown/marked.js', condition: function () {
-            return !!document.querySelector('[data-markdown]');
-        } },
-        { src: 'plugin/markdown/markdown.js', condition: function () {
-            return !!document.querySelector('[data-markdown]');
-        } },
-        { src: 'plugin/highlight/highlight.js', async: true, callback: function () {
-            hljs.initHighlightingOnLoad();
-        } },
-        { src: 'plugin/zoom-js/zoom.js', async: true, condition: function () {
-            return !!document.body.classList;
-        } },
-        { src: 'plugin/notes/notes.js', async: true, condition: function () {
-            return !!document.body.classList;
-        } },
-        { src: 'plugin/ace/ace.js', async: true, condition: function () {
-            return !!document.querySelector('.editor');
-        } }
-    ]
-});
+        // Optional libraries used to extend on reveal.js
+        dependencies: [
+            { src: 'plugin/sample-loader/loader.js', async: false},
+            { src: 'lib/js/classList.js', condition: function () {
+                return !document.body.classList;
+            } },
+            { src: 'plugin/markdown/marked.js', condition: function () {
+                return !!document.querySelector('[data-markdown]');
+            } },
+            { src: 'plugin/markdown/markdown.js', condition: function () {
+                return !!document.querySelector('[data-markdown]');
+            } },
+            { src: 'plugin/highlight/highlight.js', async: true, callback: function () {
+                hljs.initHighlightingOnLoad();
+            } },
+            { src: 'plugin/zoom-js/zoom.js', async: true, condition: function () {
+                return !!document.body.classList;
+            } },
+            { src: 'plugin/notes/notes.js', async: true, condition: function () {
+                return !!document.body.classList;
+            } },
+            { src: 'plugin/ace/ace.js', async: true, condition: function () {
+                return !!document.querySelector('.editor');
+            } }
+        ]
+    });
+
+    function callIfExist(funcName, event) {
+        var hasFunction = typeof window[funcName] == "function";
+        if (hasFunction) window[funcName](event);
+        return hasFunction;
+    }
+
+    Reveal.addEventListener('ready', function (event) {
+        var id = event.currentSlide.getAttribute('id');
+        callIfExist(id + "_onReady", event);
+        callIfExist(id + "_onChanged", event);
+    });
+    Reveal.addEventListener('slidechanged', function (event) {
+        var id = event.currentSlide.getAttribute('id');
+        callIfExist(id + "_onChanged", event);
+    });
+})();
